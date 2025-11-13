2b5f5c6f5a4d2e2b7f317c65d7999634f43e7564
Rafael Varela <rafa_varela@yahoo.com>
2025-11-13 11:00:14 -0500

    moved UpdateItem method from Program to CheeseMongersItem

--- Cheesemongers/Model/CheeseMongersItem.cs
+++ Cheesemongers/Model/CheeseMongersItem.cs
@@ -5,4 +5,91 @@ public class CheeseMongersItem
     public string Name { get; set; }
     public int ValidByDays { get; set; }
     public int Quality { get; set; }
+
+    public void UpdateItem()
+    {
+        if (Name != "Parmigiano Regiano" && Name != "Tasting with Chef Massimo")
+        {
+            if (Quality > 0)
+            {
+                if (Name != "Caciocavallo Podolico")
+                {
+                    Quality = Quality - 1;
+                }
+            }
+        }
+        else
+        {
+            if (Quality < 100)
+            {
+                Quality = Quality + 1;
+
+                if (Name == "Tasting with Chef Massimo")
+                {
+                    if (ValidByDays < 15)
+                    {
+                        if (Quality < 100)
+                        {
+                            if (Quality + 2 <= 100)
+                            {
+                                Quality = Quality + 2;
+                            }
+                            else
+                            {
+                                Quality = 100;
+                            }
+                        }
+                    }
+
+                    if (ValidByDays < 8)
+                    {
+                        if (Quality < 100)
+                        {
+                            if (Quality + 2 <= 100)
+                            {
+                                Quality = Quality + 2;
+                            }
+                            else
+                            {
+                                Quality = 100;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (Name != "Caciocavallo Podolico")
+        {
+            ValidByDays = ValidByDays - 1;
+        }
+
+        if (ValidByDays < 0)
+        {
+            if (Name != "Parmigiano Regiano")
+            {
+                if (Name != "Tasting with Chef Massimo")
+                {
+                    if (Quality > 0)
+                    {
+                        if (Name != "Caciocavallo Podolico")
+                        {
+                            if (Quality - 4 > 0)
+                            {
+                                Quality = Quality - 4;
+                            }
+                            else
+                            {
+                                Quality = 0;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Quality = 0;
+                }
+            }
+        }
+    }
 }
--- Cheesemongers/Program.cs
+++ Cheesemongers/Program.cs
@@ -15,92 +15,10 @@ public class Program
     {
         foreach (CheeseMongersItem item in Items)
         {
-            if (item.Name != "Parmigiano Regiano" && item.Name != "Tasting with Chef Massimo")
-            {
-                if (item.Quality > 0)
-                {
-                    if (item.Name != "Caciocavallo Podolico")
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            }
-            else
-            {
-                if (item.Quality < 100)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Tasting with Chef Massimo")
-                    {
-                        if (item.ValidByDays < 15)
-                        {
-                            if (item.Quality < 100)
-                            {
-                                if (item.Quality + 2 <= 100)
-                                {
-                                    item.Quality = item.Quality + 2;
-                                }
-                                else
-                                {
-                                    item.Quality = 100;
-                                }
-                            }
-                        }
-
-                        if (item.ValidByDays < 8)
-                        {
-                            if (item.Quality < 100)
-                            {
-                                if (item.Quality + 2 <= 100)
-                                {
-                                    item.Quality = item.Quality + 2;
-                                }
-                                else
-                                {
-                                    item.Quality = 100;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != "Caciocavallo Podolico")
-            {
-                item.ValidByDays = item.ValidByDays - 1;
-            }
-
-            if (item.ValidByDays < 0)
-            {
-                if (item.Name != "Parmigiano Regiano")
-                {
-                    if (item.Name != "Tasting with Chef Massimo")
-                    {
-                        if (item.Quality > 0)
-                        {
-                            if (item.Name != "Caciocavallo Podolico")
-                            {
-                                if (item.Quality - 4 > 0)
-                                {
-                                    item.Quality = item.Quality - 4;
-                                }
-                                else
-                                {
-                                    item.Quality = 0;
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        item.Quality = 0;
-                    }
-                }
-            }
+            item.UpdateItem();
         }
     }
-
+       
     static void Main(string[] args)
     {
 
